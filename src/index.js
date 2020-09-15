53933d5c4510c00cea1f06f103d71109ee3644c2
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-15 14:29:29 +0100

    solve jguery error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import './styles/main.scss'
 import 'bootstrap'
 
+var jQuery = require('jquery')
+
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
-})
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
