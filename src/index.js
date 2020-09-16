56b7bb2e94004ef7bcad2ca77661a7ba633605fb
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-16 23:55:59 +0100

    Comment Jquery line in index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import 'bootstrap';
 import './styles/main.scss';
 
 import { domContent } from './js/domContent';
-
-const jQuery = require('jquery');
-
-
 /* eslint-disable no-multi-assign, no-undef */
-// include jQuery in global and window scope (so you can access it globally)
-// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
-global.$ = global.jQuery = jQuery;
-window.$ = window.jQuery = jQuery;
+
+
+// const jQuery = require('jquery');
+
+
+// global.$ = global.jQuery = jQuery;
+// window.$ = window.jQuery = jQuery;
 
 
 $('#myModal').on('shown.bs.modal', () => {
